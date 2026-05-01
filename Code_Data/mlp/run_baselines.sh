e6f268bf0ab91ef36b721a8d87391e5d6214e5a5
Elizabeth Kourbatski <152100832+lizabatski@users.noreply.github.com>
2026-05-01 13:07:32 -0400

    Add SLURM script to run PPO baselines

---
+++ Code_Data/mlp/run_baselines.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=ppo_large_all
+#SBATCH --output=logs/out_%j.txt
+#SBATCH --error=logs/err_%j.txt
+#SBATCH --time=12:00:00
+#SBATCH --partition=all
+#SBATCH --account=winter2026-comp579
+#SBATCH --qos=comp579-1gpu-12h
+#SBATCH --mem=16G
+
+mkdir -p logs
+
+source rl579/bin/activate
+
+python run_baselines.py \
+    --save-dir ./results_ppo_large \
+    --algo ppo \
+    --envs invpend_full invpend_partial cheetah_full cheetah_partial swimmer_full \
+    --timesteps 1000000
